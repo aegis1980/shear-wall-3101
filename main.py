d97c341f5ecbb8f0179179fe228d3cc5745f2f45
aegis1980 <jonrobinson1980@gmail.com>
2021-05-15 19:11:54 +1200

    quite a bit of stuff

--- main.py
+++ main.py
@@ -1,4 +1,89 @@
-import shearwall 
+from shearwall import ShearWall,AnalysisType, BAR_SIZES, interaction_curve
 import streamlit as st
+import plotly.graph_objects as go
+
+st.sidebar.markdown(
+    "Experimental webapp for design of shear wall to NZS3101." \
+    "Everything here is entirely untested, so this must not be used for engineering design or verification." 
+)
+st.sidebar.markdown(
+    "Code is here." \
+    "Two requests:" \
+    " If you find an error or add a feature, please "
+    " Respect the license"     
+)
+st.sidebar.subheader("")
+st.sidebar.markdown("[Analysis type](#analysis-type)")
+st.sidebar.markdown("[Design actions](#design-actions)")
+st.sidebar.markdown("[Wall dimensions](#wall-dimensions)")
+st.sidebar.markdown("[Concrete props](#concrete)")
+
+
+st.header('Analysis type')
+atype=st.selectbox(label ='', options = ['Elastic', 'Limited ductile', 'Fully ductile'])
+
+st.header('Design actions')
+n_column, m_column, v_column = st.beta_columns(3)
+with n_column:
+    axial = st.number_input(label='ULS Axial load (kN)',value =2500)
+with m_column:    
+    moment = st.number_input(label='ULS Moment (kNm)',value =21000)
+with v_column:  
+    shear = st.number_input(label='ULS Shear (kN)',value =220)
+
+st.header('Wall dimensions')
+t_column, l_column = st.beta_columns(2)
+with t_column:
+    t = st.number_input(label='Enter wall thickness (mm)',value =200)
+with l_column:
+    l_w = st.number_input(label='Enter wall length (mm)', value = 3000)
+    
+
+
+st.header('Concrete')
+fc_column, c_column = st.beta_columns(2)
+with fc_column:
+    f_c = st.number_input(label='Compression strength (MPa)', value = 40)
+with c_column:
+    c_end = st.number_input(label='Cover to steel (mm)', value = 40)
+
+st.header('Reo')
+v_column, h_column, s_column = st.beta_columns(3)
+with v_column:
+    st.subheader('Vertical steel')
+    d_bl = st.selectbox(label= 'Dia vertical steel (mm)', options = BAR_SIZES)
+    f_y = st.number_input(label='Vert reo yield strength (MPa)', value = 500)
+    s_v = st.number_input(label='Spacing of vertical bars (mm)', value = 300)
+    n_l = st.number_input(label='Number of layers steel', value =2)
+with h_column:
+    st.subheader('Horizontal steel')
+    d_bh = st.selectbox(label= 'Dia horizontal steel (mm)', options = BAR_SIZES)
+    f_yt = st.number_input(label='Horiz reo yield strength (MPa)', value = 500)
+with s_column:
+    st.subheader('Stirrups')
+    d_s = st.selectbox(label= 'Dia stirrups (mm)', options = BAR_SIZES)
+    f_ys = st.number_input(label='Stirrups yield strength (MPa)', value = 500)
+
+
+m,n = interaction_curve(
+    atype=atype,
+    t = t, 
+    l_w = l_w,
+    f_c=f_c,
+    f_y=f_y,
+    f_yt=f_yt,
+    d_bl=d_bl,
+    d_s=8,
+    s_v=s_v,
+    n_l=n_l,
+    c_end=c_end
+)
+
+fig = go.Figure(data = go.Scatter(x = m,y=n))
+
+st.plotly_chart(fig, use_container_width= True)
+
+
+
 
 
--- shearwall/__init__.py
+++ shearwall/__init__.py
@@ -1,8 +1,9 @@
-from typing import List
+
+from typing import List, Tuple
 import math 
 from enum import Enum
-from platform import mac_ver
 
+from scipy.optimize import fsolve
 
 DEFAULTS ={
     'sci_v' : 0.75
@@ -25,10 +26,12 @@ class ShearWall:
            f_c : int,
            f_y : int,
            d_bl : int, 
+           d_s: int,
            s_v : int,
            n_l:int,
            c_end: int, 
            axial_load: float = 0,
+           atype: AnalysisType = AnalysisType.LIMITED_DUCTILE,
            f_yt : int = None,
         ):
             self.l_w = l_w
@@ -37,10 +40,12 @@ class ShearWall:
             self.f_y = f_y
             self.f_yt = f_yt or f_y
             self.d_bl = d_bl
+            self.d_s = d_s # dia of stirrups
             self.s_v = s_v
             self.n_l :int= n_l
             self.c_end : int = c_end 
             self.Nu  = axial_load
+            self.atype = atype
 
 
 
@@ -65,10 +70,10 @@ class ShearWall:
 
         """
         max_d: int = int(self.t/10)
-        if self.type == AnalysisType.ELASTIC:
+        if self.atype == AnalysisType.ELASTIC:
             max_d  = int(self.t/7)
             msg = "For elastic analysis, bar dia limited to (wall thickness / 7)"
-        elif self.type == AnalysisType.LIMITED_DUCTILE:
+        elif self.atype == AnalysisType.LIMITED_DUCTILE:
             max_d = int(self.t/8)
             msg = "For limited ductile analysis, bar dia limited to (wall thickness / 8)"
         else: 
@@ -81,16 +86,22 @@ class ShearWall:
             self.d_bl = max_d
             return False
         else:
-            self.logs.append('Bar diameter okay')
+            self.logs.append(f'Bar diameter okay, unchanged from {self.d_bl}')
             self.logs.append(msg)
             return True
             
 
 
     def check_sv_max(self):
-        if self.t * 3 > 450:
-            self.t = 450
+        s1 = 3 * self.t
+        s2 = 450
+        smax= max(s1,s2)
+        if self.s_v > smax :
+            self.s_v = smax
             self.warnings.append("Vertical spacing limited to lesser of (3 x wall thickness) & 450mm")
+            self.warnings.append(f'Spacing reduced to {smax}')
+            return False
+        return True
 
     def calc_a_s(self):
         """
@@ -107,7 +118,8 @@ class ShearWall:
         Returns:
             area : calculated long'l wall reo 
         """
-        self.a_s =  self.n_l*(self.d_bl**2)*math.pi*self.lw / ( 4*self.s_v)
+        self.a_s =  self.n_l*(self.d_bl**2)*math.pi*self.l_w / ( 4*self.s_v)
+        self.logs.append(f'Area long steel, As= {self.a_s}')
         return self.a_s
 
 
@@ -115,21 +127,15 @@ class ShearWall:
         """ calculates bar positions
 
         """
-        l_avail = self.l_w - (2*(self.c_end + self.d_v)+ self.d_bl)
-        n_bars = math.ceiling(l_avail / self.s_v)
-        l_occ = self.s_v * n_bars
+        l_avail = self.l_w - (2*(self.c_end + self.d_s)+ self.d_bl)
+        n_bars = math.floor(l_avail / self.s_v) +1
+        l_occ = self.s_v * (n_bars-1)
         x_0 = (self.l_w - l_occ)/ 2 # centre the n bar in available space. this is first bar position.
         
         for i in range(n_bars):
             x = x_0 + self.s_v * i
             self.bar_x.append(x)
         
-
-
-
-
-
-
 
     def calc_rho_n(self):
         """
@@ -139,6 +145,7 @@ class ShearWall:
             [type]: [description]
         """
         self.rho_n = self.a_s/(self.t*self.l_w)
+        self.logs.append(f'Vertical reo ratio {self.rho_n}')
         return self.rho_n
 
 
@@ -152,10 +159,11 @@ class ShearWall:
             self.error.append('Something wrong with checking min vertical reo ratio')
             return False
         elif self.rho_n < rho_min:
-            self.error.append('Vertical reo area < min allowable')
+            self.errors.append('Vertical reo area < min allowable')
             return False
         self.logs.append('Vertical reo ratio > min allowed. ok.')
         return True
+
 
     def calc_a1_b1(self):
         if self.f_c <=55:
@@ -168,16 +176,18 @@ class ShearWall:
         if self.f_c < 30:
             self.b1 = 0.85
         elif self.f_c < 55:
-            self.b1 = 0.85 - 0.004*(self.f_c-30)
+            self.b1 = 0.85 - 0.008*(self.f_c-30)
         else: 
             self.b1 =0.65
-        
+        self.logs.append(f'alpha1 = {self.a1}')
+        self.logs.append(f'beta1 = {self.b1}')
         return self.a1,self.b1
 
 
-    def solve_neutral_axis(self) -> int:
+    def solve(self,limit: float =0.1) -> int:
         a1, b1 = self.calc_a1_b1()
         n_bars = len(self.bar_x)
+        self.logs.append(f'Number of bars {n_bars}')
 
         tension = [True] * n_bars
         e_s = [0] * n_bars
@@ -185,85 +195,153 @@ class ShearWall:
         F_s = [0] * n_bars
         M_s = [0] * n_bars
 
-        c = self.l_w / 2
+        Cc =0
 
-        for i in range(n_bars):
-            tension[i] = self.bar_x[i] > c
-            if tension[i]:
-                e_s[i] = 0.003*(self.bar_x[i]-c)/ c
+        def solve_fn(x, report = False):
+            for i in range(n_bars):
+                tension[i] = self.bar_x[i] > x
+               
+                if tension[i]:
+                    e_s[i] = 0.003*(self.bar_x[i]-x)/ x
+                else:
+                    e_s[i] = 0
+                    
+
+                f_s[i] = min(e_s[i] * 200000,self.f_y) # NOTE Shouldnt this be factored Mpa 
+                F_s[i] = f_s[i] * (self.d_bl**2) * math.pi * self.n_l /4000 #kN
+                M_s[i] = F_s[i] * (self.bar_x[i]-0.5 * b1 * x)/1000
+
+            Ts = sum(F_s) # kN
+            Cc = x * a1 * b1 * self.f_c * self.t /1000 #kN shooulnt f_c be factored?      
+            Ms = sum(M_s)
+            
+            if not report:
+                return (Ts+ self.Nu) - Cc # for force equilbirum
             else:
-                e_s[i] = 0
-                
-
-            f_s[i] = max(e_s[i] * 200000,self.f_y) # NOTE Shouldnt this be factored Mpa 
-            F_s[i] = f_s[i] * (self.d_bl**2) * math.pi * self.n_l /4000 #kN
-            M_s[i] = F_s[i] * (self.bar_x[i]-0.5 * b1 * c)/1000
-
-        Ts = sum(F_s) # kN
-        Cc = c * a1 * b1 * self.f_c * self.t /1000 #kN shooulnt f_c be factored?      
-
-        Ms = sum(M_s)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                return tension, e_s, f_s, F_s, M_s, Ts, Cc, Ms
         
+        x_na = fsolve(solve_fn,[self.l_w/2])[0]
+
+        tension, e_s, f_s, F_s, M_s, Ts, Cc, Ms = solve_fn(x_na,True)
+
+        Mn = self.Nu*(0.5*self.l_w - (0.5 * b1* x_na))/1000
+
+        self.th_Mn= 0.85 *(Ms + Mn)
+        
+        return self.th_Mn
+
+    def shear(self):
+        v_n = 1000 * self.Vu /(0.75 * 0.8 * self.t * self.l_w)
+        v_max = min(0.2 * self.f_c,8)
+        Vc = 0
+
+
+# - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] - - [] -
+
+def interaction_curve(
+    t : int, 
+    l_w : int,
+    f_c : int,
+    f_y : int,
+    d_bl : int, 
+    d_s: int,
+    s_v : int,
+    n_l:int,
+    c_end: int, 
+    atype: AnalysisType = AnalysisType.LIMITED_DUCTILE,
+    f_yt : int = None
+) -> Tuple[List[float],List[float]]:
+
+    N = []
+    M = []
+    m=0
+    last_m = 0
+    n=0
+    count = 0
+    count_limit = 20
+    while True:        
+        sw = ShearWall(
+            atype=atype,
+            t = t, 
+            l_w = l_w,
+            f_c=f_c,
+            f_y=f_y,
+            d_bl=d_bl,
+            d_s=8,
+            s_v=s_v,
+            n_l=n_l,
+            c_end=c_end,
+            axial_load=n
+        )
+        sw.update()
+        last_m = m
+        m = sw.solve(limit = 1)
+        if last_m<m: 
+            count_limit += 1
+        N.append(n)
+        M.append(m)
+        n +=500
+        count +=1
+
+        if count>count_limit or m<0:
+            break
+    return M,N
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
