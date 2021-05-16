905605f96e41ee5c2634e1a8af0801b37adfaa47
aegis1980 <jonrobinson1980@gmail.com>
2021-05-16 14:06:04 +1200

    layout change

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ st.set_page_config(
      initial_sidebar_state="expanded",
 )
 
-st.sidebar.markdown("Experimental webapp for design of shear wall to NZS3101.")
+st.sidebar.markdown("Experimental web-app for design of shear wall to NZS3101.")
 st.sidebar.markdown("Everything here is entirely untested, so not to be for anything other than play! 😵")
 st.sidebar.markdown("Code is [here](https://github.com/aegis1980/shear-wall-3101). If you find an error or add a feature, **please** make a pull request.")     
 st.sidebar.markdown('''
@@ -40,66 +40,57 @@ st.sidebar.markdown("---")
 
 st.sidebar.markdown("[1. Analysis type](#analysis-type)")
 st.sidebar.markdown("[2. Design actions](#design-actions)")
-st.sidebar.markdown("[3. Wall dimensions](#wall-dimensions)")
-st.sidebar.markdown("[4. Concrete props](#concrete)")
-st.sidebar.markdown("[5. Reo props](#reo)")
+st.sidebar.markdown("[3. Wall dimensions](#dims)")
+st.sidebar.markdown("[4. Concrete properties](#concrete)")
+st.sidebar.markdown("[5. Reinforcement properties](#reo)")
 st.sidebar.markdown("---")
 st.sidebar.markdown("[Results](#results)")
 
-at_column, left_column = st.beta_columns([1,3])
-with at_column:
-    st.subheader('Analysis type')
-with left_column:
-    atype=st.selectbox(label ='Select analysis type', options = ['Elastic', 'Limited ductile', 'Fully ductile'])
+st.subheader('Analysis type')
+atype=st.selectbox(label ='Select analysis type', options = ['Elastic', 'Limited ductile', 'Fully ductile'])
 
-
-da_column,n_column, m_column, v_column = st.beta_columns(4)
-
-with da_column:
-    st.subheader('Design actions') 
+st.subheader('Design actions')
+n_column, m_column, v_column = st.beta_columns(3)
+ 
 with n_column:
-    axial = st.text_input(label='ULS Axial load (kN)',value =2500)
+    axial = st.text_input(label='ULS Axial load (kN)',value =3000)
     axial = str_to_int(axial)
 with m_column:    
-    moment = st.number_input(label='ULS Moment (kNm)',value =21000)
+    moment = st.number_input(label='ULS Moment (kNm)',value =2100)
 with v_column:  
-    shear = st.number_input(label='ULS Shear (kN)',value =220)
+    shear = st.number_input(label='ULS Shear (kN)',value =300)
 
-
-wd_column,t_column, l_column , blank1= st.beta_columns(4)
-with wd_column:
-    st.subheader('Wall dims')
+st.subheader('Wall dimensions', anchor = 'dims')
+t_column, l_column , blank1= st.beta_columns(3)  
 with t_column:
     t = st.number_input(label='Enter wall thickness (mm)',value =200)
 with l_column:
     l_w = st.number_input(label='Enter wall length (mm)', value = 3000)
     
 
-sp_column,fc_column, c_column , blank2= st.beta_columns(4)
-
-with sp_column:
-    st.subheader('Concrete')
+st.subheader('Concrete properties', anchor = 'concrete')
+fc_column, c_column , blank2= st.beta_columns(3)
 with fc_column:
     f_c = st.number_input(label='Compression strength (MPa)', value = 40)
 with c_column:
     c_end = st.number_input(label='Cover to steel (mm)', value = 40)
 
-
-reo_column, v_column, h_column, s_column = st.beta_columns(4)
-with reo_column:
-    st.subheader('Reo')
+st.subheader('Reinforcment properties', anchor = 'reo')
+v_column, h_column, s_column = st.beta_columns(3)
 with v_column:
-    st.subheader('Vertical steel')
-    d_bl = st.selectbox(label= 'Dia vertical steel (mm)', options = BAR_SIZES)
+    st.markdown('**Vertical steel**')
+    d_bl = st.selectbox(label= 'Dia vertical steel (mm)', options = BAR_SIZES,index = 4)
     f_y = st.number_input(label='Vert reo yield strength (MPa)', value = 500)
     s_v = st.number_input(label='Spacing of vertical bars (mm)', value = 300)
-    n_l = st.number_input(label='Number of layers steel', value =2, help="e.g. If single layer of vertical reo in front of wall and single layer in back, then n=2")
+    n_l = st.number_input(label='Number of layers steel', value =2, help="e.g. If single layer of vertical reo in front of wall and single layer in back, then **n=2**")
 with h_column:
-    st.subheader('Horizontal steel')
-    d_bh = st.selectbox(label= 'Dia horizontal steel (mm)', options = BAR_SIZES)
+    st.markdown('**Horizontal steel**')
+    d_bh = st.selectbox(label= 'Dia horizontal steel (mm)', options = BAR_SIZES, index = 2)
     f_yt = st.number_input(label='Horiz reo yield strength (MPa)', value = 500)
+    s_h = st.number_input(label='Spacing of horizontal bars (mm)', value = 300)
+   
 with s_column:
-    st.subheader('Stirrups')
+    st.markdown('**Stirrups**')
     d_s = st.selectbox(label= 'Dia stirrups (mm)', options = BAR_SIZES)
     f_ys = st.number_input(label='Stirrups yield strength (MPa)', value = 500)
 
